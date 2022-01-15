0f7e8007105ab7c66100601a24be08b599a5ab8e
Fillipe <fillipe.janiques@gmail.com>
2022-01-15 11:53:55 -0300

    added colors

--- Entities/Enemy/Enemy.gd
+++ Entities/Enemy/Enemy.gd
@@ -1,7 +1,30 @@
 extends RigidBody2D
 
-class_name Enemy
+var color
+var spawnColor
 
-var r = Color(1, 0, 0)
-var g = Color(0, 1, 0)
-var b = Color(0, 0, 1)
+enum {
+	RED,
+	GREEN,
+	BLUE
+}
+
+var speed
+
+func _ready():
+	_set_color()
+	match color:
+		RED:
+			modulate = Color(1, 0, 0)
+		GREEN:
+			modulate = Color(0, 1, 0)
+		BLUE:
+			modulate = Color(0, 0, 1)
+
+func _set_color():
+	if(spawnColor == 1):
+		color = RED
+	elif(spawnColor == 2):
+		color = GREEN
+	else:
+		color = BLUE
--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -3,12 +3,16 @@ extends Control
 var enemy = preload("res://Entities/Enemy/Enemy.tscn")
 
 var spawn
+var spawnColor
 onready var spawnTimer = $Timer
+var enemySpeed :float = 10
 
 func _ready():
 	randomize()
 
 func _on_Timer_timeout():
 	spawn = int(rand_range(1,4))
+	spawnColor = int(rand_range(1,4))
 	var enemyInstance = enemy.instance()
+	enemyInstance.spawnColor = spawnColor
 	get_node("VBox/Spawners/Spawn" + str(spawn)).add_child(enemyInstance)
